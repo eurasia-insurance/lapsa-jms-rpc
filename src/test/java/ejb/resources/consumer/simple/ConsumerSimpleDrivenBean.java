2a9826f5590eeb3fddbd1827ce7b6582f902c650
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-24 09:42:17 +0600

    consumer with properties test

--- src/test/java/ejb/resources/consumer/simple/ConsumerSimpleDrivenBean.java
+++ src/test/java/ejb/resources/consumer/simple/ConsumerSimpleDrivenBean.java
@@ -4,11 +4,15 @@ import java.util.Properties;
 
 import javax.ejb.MessageDriven;
 
+import tech.lapsa.java.commons.function.MyObjects;
+import tech.lapsa.java.commons.function.MyStrings;
 import tech.lapsa.javax.jms.ObjectConsumerDrivenBean;
 import test.ConsumerSimpleTest;
 
 @MessageDriven(mappedName = ConsumerSimpleDestination.JNDI_NAME)
 public class ConsumerSimpleDrivenBean extends ObjectConsumerDrivenBean<ConsumerSimpleEntity> {
+
+    public static final String PROPERTY_NAME = "name";
 
     public ConsumerSimpleDrivenBean() {
 	super(ConsumerSimpleEntity.class);
@@ -16,7 +20,12 @@ public class ConsumerSimpleDrivenBean extends ObjectConsumerDrivenBean<ConsumerS
 
     @Override
     protected void accept(ConsumerSimpleEntity entity, Properties properties) {
-	ConsumerSimpleTest.BASIC_EXPECTED = entity;
+	if (MyObjects.nonNull(properties)) {
+	    final String name = properties.getProperty(PROPERTY_NAME);
+	    if (MyStrings.nonEmpty(name))
+		ConsumerSimpleTest.WITH_PROPERTIES_EXPECTED = name;
+	}
+	if (MyObjects.nonNull(entity))
+	    ConsumerSimpleTest.BASIC_EXPECTED = entity;
     }
-
 }
--- src/test/java/test/ConsumerSimpleTest.java
+++ src/test/java/test/ConsumerSimpleTest.java
@@ -3,12 +3,15 @@ package test;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.util.Properties;
+
 import javax.inject.Inject;
 import javax.jms.JMSException;
 
 import org.junit.Test;
 
 import ejb.resources.consumer.simple.ConsumerSimpleDestination;
+import ejb.resources.consumer.simple.ConsumerSimpleDrivenBean;
 import ejb.resources.consumer.simple.ConsumerSimpleEntity;
 import tech.lapsa.javax.jms.MyJMSClient;
 import tech.lapsa.javax.jms.MyJMSClient.MyJMSConsumer;
@@ -25,14 +28,34 @@ public class ConsumerSimpleTest extends ArquillianBaseTestCase {
 
     @Test
     public void basic() throws JMSException {
-	final MyJMSConsumer<ConsumerSimpleEntity> function = jmsClient
+	final MyJMSConsumer<ConsumerSimpleEntity> consumer = jmsClient
 		.createConsumer(consumerSimpleDestination.getDestination());
 	{
 	    final String MESSAGE = "Hello JMS world!";
 
 	    final ConsumerSimpleEntity e = new ConsumerSimpleEntity(MESSAGE);
-	    function.accept(e);
+	    consumer.accept(e);
 	    assertThat(BASIC_EXPECTED, allOf(not(nullValue()), is(equalTo(e))));
+	}
+    }
+
+    public static String WITH_PROPERTIES_EXPECTED = null;
+
+    @Test
+    public void withProperties() throws JMSException {
+	final MyJMSConsumer<ConsumerSimpleEntity> consumer = jmsClient
+		.createConsumer(consumerSimpleDestination.getDestination());
+	{
+	    final String MESSAGE = "Hello, %1$s!";
+	    final String NAME = "John Bull";
+
+	    final Properties properties = new Properties();
+	    properties.setProperty(ConsumerSimpleDrivenBean.PROPERTY_NAME, NAME);
+
+	    final ConsumerSimpleEntity e = new ConsumerSimpleEntity(MESSAGE);
+	    consumer.accept(e, properties);
+	    assertThat(WITH_PROPERTIES_EXPECTED, allOf(not(nullValue()), is(equalTo(NAME))));
+
 	}
     }
 }
