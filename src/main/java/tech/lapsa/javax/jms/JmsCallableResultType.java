bcbfd71269b08e392b972124e0a0971179468b09
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-29 14:27:15 +0600

    annotations

--- src/main/java/tech/lapsa/javax/jms/JmsCallableResultType.java
+++ src/main/java/tech/lapsa/javax/jms/JmsCallableResultType.java
@@ -1,7 +1,14 @@
 package tech.lapsa.javax.jms;
 
-import java.io.Serializable;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
 
+import java.io.Serializable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ FIELD, TYPE, METHOD })
 public @interface JmsCallableResultType {
     Class<? extends Serializable> value();
 }
--- src/main/java/tech/lapsa/javax/jms/JmsDestinationMappedName.java
+++ src/main/java/tech/lapsa/javax/jms/JmsDestinationMappedName.java
@@ -6,7 +6,6 @@ import static java.lang.annotation.RetentionPolicy.*;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-//@Qualifier
 @Retention(RUNTIME)
 @Target({ FIELD, TYPE, METHOD })
 public @interface JmsDestinationMappedName {
