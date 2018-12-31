93df53c8f622a1698b07fa37a06db7f4a8cad134
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-12-31 13:06:29 +0300

    name and separate test to get more stable

--- src/test/java/test/CallableWithPropertiesTest.java
+++ src/test/java/test/CallableWithPropertiesTest.java
@@ -3,19 +3,22 @@ package test;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.util.Properties;
+
 import javax.inject.Inject;
 import javax.jms.JMSException;
 
 import org.junit.Test;
 
 import ejb.resources.callable.simple.CallableSimpleDestination;
+import ejb.resources.callable.simple.CallableSimpleDrivenBean;
 import ejb.resources.callable.simple.CallableSimpleEntity;
 import ejb.resources.callable.simple.CallableSimpleResult;
 import tech.lapsa.lapsa.jmsRPC.client.JmsCallableClient;
 import tech.lapsa.lapsa.jmsRPC.client.JmsDestination;
 import tech.lapsa.lapsa.jmsRPC.client.JmsResultType;
 
-public class CallableSimpleTest extends ArquillianBaseTestCase {
+public class CallableWithPropertiesTest extends ArquillianBaseTestCase {
 
     @Inject
     @JmsDestination(CallableSimpleDestination.GENERAL)
@@ -25,11 +28,15 @@ public class CallableSimpleTest extends ArquillianBaseTestCase {
     @Test
     public void basic() throws JMSException {
 	{
-	    final String MESSAGE = "Hello JMS world!";
-	    final String EXPECTING_MESSAGE = CallableSimpleResult.PREFIX + MESSAGE;
+	    final String MESSAGE = "Hello, %1$s!";
+	    final String NAME = "John Bull";
+	    final String EXPECTING_MESSAGE = CallableSimpleResult.PREFIX + "Hello, " + NAME + "!";
+
+	    final Properties properties = new Properties();
+	    properties.setProperty(CallableSimpleDrivenBean.PROPERTY_NAME, NAME);
 
 	    final CallableSimpleEntity e = new CallableSimpleEntity(MESSAGE);
-	    final CallableSimpleResult r = callableClient.call(e);
+	    final CallableSimpleResult r = callableClient.call(e, properties);
 	    assertThat(r, not(nullValue()));
 	    assertThat(r.getMessage(),
 		    allOf(not(nullValue()), is(equalTo(EXPECTING_MESSAGE))));
